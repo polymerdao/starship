816512d816940608a8e0d13f3bd5e06acc2e6a3e
inkvi <374203+Inkvi@users.noreply.github.com>
2023-07-25 17:08:18 -0700

    Extend address script to return a cosmos address by mnemonic

--- charts/devnet/contracts/address.js
+++ charts/devnet/contracts/address.js
@@ -1,10 +1,46 @@
-const ethers = require('ethers');
+const {Secp256k1HdWallet} = require("@cosmjs/launchpad");
 const {program} = require('commander');
+const ethers = require('ethers');
 
-program.requiredOption("-m, --mnemonic <mnemonic>", "Your mnemonic");
+program
+    .requiredOption("-m, --mnemonic <mnemonic>", "Your mnemonic")
+    .option("-p, --prefix <prefix>", "The bech32 prefix of the chain", "cosmos");
 program.parse(process.argv);
 
 const opts = program.opts();
-const wallet = ethers.Wallet.fromMnemonic(opts.mnemonic);
-const address = wallet.address;
-console.log(address);
+
+async function generateCosmosAddress(mnemonic, hrp) {
+    // Create a wallet from the mnemonic with a custom hrp
+    const wallet = await Secp256k1HdWallet.fromMnemonic(mnemonic, {prefix: hrp});
+
+    // Get accounts from the wallet
+    const accounts = await wallet.getAccounts();
+
+    // Assuming you want the first account
+    const {address, pubkey} = accounts[0];
+
+    // Convert pubkey to a hex string
+    const pubkeyHexString = Buffer.from(pubkey).toString('hex');
+
+    return {cosmosAddress: address, cosmosPubkey: pubkeyHexString};
+}
+
+function generateEthereumAddress(mnemonic) {
+    const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+    return {
+        ethereumAddress: wallet.address,
+        ethereumPrivateKey: wallet.privateKey
+    };
+}
+
+(async function () {
+    const cosmosData = await generateCosmosAddress(opts.mnemonic, opts.prefix);
+    const ethereumData = generateEthereumAddress(opts.mnemonic);
+
+    const combinedData = {
+        ...cosmosData,
+        ...ethereumData
+    };
+
+    console.log(JSON.stringify(combinedData));
+})();
