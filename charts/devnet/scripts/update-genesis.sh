7ffe1f9c6cde12a7488efd2c273274c366f69e1d
inkvi <374203+Inkvi@users.noreply.github.com>
2023-09-12 15:57:04 -0700

    Update bonding time and client trusting period

--- charts/devnet/scripts/update-genesis.sh
+++ charts/devnet/scripts/update-genesis.sh
@@ -13,7 +13,7 @@ sed -i -e "s/\"stake\"/\"$DENOM\"/g" $CHAIN_DIR/config/genesis.json
 sed -i "s/\"time_iota_ms\": \".*\"/\"time_iota_ms\": \"$TIME_IOTA_MS\"/" $CHAIN_DIR/config/genesis.json
 
 echo "Update staking unbonding time and slashing jail time"
-jq -r '.app_state.staking.params.unbonding_time |= "90s"' $CHAIN_DIR/config/genesis.json > /tmp/genesis.json; mv /tmp/genesis.json $CHAIN_DIR/config/genesis.json
+jq -r '.app_state.staking.params.unbonding_time |= "24h"' $CHAIN_DIR/config/genesis.json > /tmp/genesis.json; mv /tmp/genesis.json $CHAIN_DIR/config/genesis.json
 jq -r '.app_state.slashing.params.downtime_jail_duration |= "6s"' $CHAIN_DIR/config/genesis.json > /tmp/genesis.json; mv /tmp/genesis.json $CHAIN_DIR/config/genesis.json
 
 # overrides for older sdk versions, before 0.47
